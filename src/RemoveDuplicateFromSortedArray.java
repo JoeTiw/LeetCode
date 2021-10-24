41d91231769f85a8ba9f1d928bd2d769b3b2c7fc
JoeTiw <bhupintiwari@icloud.com>
2021-10-24 10:15:32 -0500

    LeetCode

---
+++ src/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) {
+        class Solution {
+            public int removeDuplicates(int[] nums) {
+
+                int newNums =1;
+
+                for (int i = 0; i < nums.length-1; i++)
+
+                    if( nums[i] != nums[i+1]) {    //compare to see if the next index is equal or not
+                        nums [newNums ++] = nums [i + 1];  //if not we increment newNums to store that value
+                    }
+
+                return newNums;
+
+            }
+        }
+        }
+    }
+
---
+++ src/leetcode/company/Main.java
@@ -0,0 +1,9 @@
+package leetcode.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
